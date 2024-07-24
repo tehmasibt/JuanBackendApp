0e5c9a590ebdfc8b9dc7252d1cc39fef04c440d1
thmasibtagiyev0@gmail.com <tahmasibmt@code.edu.az>
2024-07-24 15:02:00 +0400

    HomeController modified

--- JuanBackendApp/Controllers/HomeController.cs
+++ JuanBackendApp/Controllers/HomeController.cs
@@ -1,4 +1,3 @@
-using JuanBackendApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,27 +5,9 @@ namespace JuanBackendApp.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
