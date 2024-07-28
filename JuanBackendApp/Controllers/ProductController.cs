94a8e6e5f1423649e594260990ddc1785436e886
thmasibtagiyev0@gmail.com <tahmasibmt@code.edu.az>
2024-07-28 18:10:17 +0400

    PartialView

---
+++ JuanBackendApp/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using JuanBackendApp.App_Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace JuanBackendApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        private readonly JuanAppDbContext _juanAppDbContext;
+        public ProductController(JuanAppDbContext juanAppDbContext)
+        {
+            _juanAppDbContext = juanAppDbContext;
+        }
+        public async Task<IActionResult> Modal(int? id)
+        {
+            if (id == null) return BadRequest();
+            var product =await _juanAppDbContext.Products
+                .AsNoTracking()
+                .Where(x => !x.IsDeleted)
+                .Include(a=>a.ProductImages)
+                .FirstOrDefaultAsync(x => x.Id == id);
+            if (product == null) return NotFound();
+            return PartialView("_ModalPartial",product);
+        }
+    }
+}
---
+++ JuanBackendApp/wwwroot/assets/js/custom.js
@@ -0,0 +1,41 @@
+﻿$(document).ready(function () {
+
+    $(".productModal").click(function (ev) {
+        ev.preventDefault();
+        let url=$(this).attr("href");
+        axios.get(url)
+            .then(function (response) {
+                // handle success
+                $(".product-details-inner").html(response.data)
+				$('.product-large-slider').slick({
+					fade: true,
+					arrows: false,
+					asNavFor: '.pro-nav'
+				});
+				// product details slider nav active
+				$('.pro-nav').slick({
+					slidesToShow: 4,
+					asNavFor: '.product-large-slider',
+					arrows: false,
+					focusOnSelect: true
+				});
+				$('.pro-qty').prepend('<span class="dec qtybtn">-</span>');
+				$('.pro-qty').append('<span class="inc qtybtn">+</span>');
+				$('.qtybtn').on('click', function () {
+					var $button = $(this);
+					var oldValue = $button.parent().find('input').val();
+					if ($button.hasClass('inc')) {
+						var newVal = parseFloat(oldValue) + 1;
+					} else {
+						// Don't allow decrementing below zero
+						if (oldValue > 0) {
+							var newVal = parseFloat(oldValue) - 1;
+						} else {
+							newVal = 0;
+						}
+					}
+					$button.parent().find('input').val(newVal);
+				});
+            })
+    })
+})
